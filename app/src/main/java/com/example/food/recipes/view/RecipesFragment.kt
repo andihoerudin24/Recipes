4595fd79ff1c9e8c5e4aa136f8c662e8eb05d6ba
Andi Hoerduin <andihoerudin24@gmail.com>
2021-10-05 23:38:43 +0700

    13 done

--- app/src/main/java/com/example/food/recipes/view/RecipesFragment.kt
+++ app/src/main/java/com/example/food/recipes/view/RecipesFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.food.R
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        view.recyclerview.showShimmer()
+        return view
     }
 }
