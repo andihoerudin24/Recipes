de71ca5d1e4701c5c12d9d3853a1f0717d1f9397
Andi Hoerduin <andihoerudin24@gmail.com>
2021-11-18 23:42:59 +0700

    73 done

---
+++ app/src/main/java/com/example/food/baseview/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.food.baseview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.food.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
