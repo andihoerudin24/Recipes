386b42d9e08eb95ff53e875e1caa1fda682605fa
Andi Hoerduin <andihoerudin24@gmail.com>
2021-11-24 23:33:09 +0700

    create pageadapter class

--- app/src/main/java/com/example/food/baseview/DetailsActivity.kt
+++ app/src/main/java/com/example/food/baseview/DetailsActivity.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.example.food.R
+import com.example.food.fragments.IngredientFragment
+import com.example.food.fragments.InstructuionsFragment
+import com.example.food.fragments.OverviewFragment
+import com.example.food.fragments.adapters.PagerAdapter
 import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
@@ -15,6 +24,29 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientFragment())
+        fragments.add(InstructuionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipesBundle", args.result)
+
+        val adapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            titles,
+            supportFragmentManager
+        )
+
+        viewePager.adapter = adapter
+        tabLayout.setupWithViewPager(viewePager)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
---
+++ app/src/main/java/com/example/food/fragments/OverviewFragment.kt
@@ -0,0 +1,20 @@
+package com.example.food.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.food.R
+
+class OverviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview, container, false)
+    }
+}
