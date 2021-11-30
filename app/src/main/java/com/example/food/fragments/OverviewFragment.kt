75b14a253879e0b5f46e9c162ecaabbad9ae974c
Andi Hoerduin <andihoerudin24@gmail.com>
2021-11-30 22:46:25 +0700

    overview fragment display recipe data & parse html text

--- app/src/main/java/com/example/food/fragments/OverviewFragment.kt
+++ app/src/main/java/com/example/food/fragments/OverviewFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import coil.load
 import com.example.food.R
+import com.example.food.recipes.model.Result
+import kotlinx.android.synthetic.main.fragment_overview.view.*
+import org.jsoup.Jsoup
 
 class OverviewFragment : Fragment() {
 
@@ -15,6 +20,48 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
+        val args = arguments
+        val mybundle: Result? = args?.getParcelable("recipesBundle")
+        view.main_imageView.load(mybundle?.image)
+        view.title_textView.text = mybundle?.title
+        view.likes_textView.text = mybundle?.aggregateLikes.toString()
+        view.time_textView.text = mybundle?.readyInMinutes.toString()
+        mybundle?.summary.let {
+            val summary = Jsoup.parse(it).text()
+            view.summary_textView.text = summary
+        }
+
+        if (mybundle?.vegetarian == true) {
+            view.vegetarian_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.vegetarian_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if (mybundle?.vegan == true) {
+            view.vegan_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.vegan_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if (mybundle?.glutenFree == true) {
+            view.gluten_free_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.gluten_free_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if (mybundle?.dairyFree == true) {
+            view.dairy_free_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.dairy_free_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if (mybundle?.veryHealthy == true) {
+            view.healthy_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.healthy_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if (mybundle?.cheap == true) {
+            view.cheap_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.cheap_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/example/food/recipes/adapter/bindingAdapter/RecipesRowBinding.kt
+++ app/src/main/java/com/example/food/recipes/adapter/bindingAdapter/RecipesRowBinding.kt
@@ -12,6 +12,7 @@ import coil.load
 import com.example.food.R
 import com.example.food.recipes.model.Result
 import com.example.food.recipes.view.RecipesFragmentDirections
+import org.jsoup.Jsoup
 
 class RecipesRowBinding {
 
@@ -74,6 +75,15 @@ class RecipesRowBinding {
                         )
                     }
                 }
+            }
+        }
+
+        @BindingAdapter("parseHtml")
+        @JvmStatic
+        fun parseHtml(textView: TextView, description: String?) {
+            if (description != null) {
+                val desc = Jsoup.parse(description).text()
+                textView.text = desc
             }
         }
     }
